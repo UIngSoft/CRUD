4aac009f6b00aea9ac7b2a60a023cb5e67bfec5a
santiago granada aguirre <32211963+santix96@users.noreply.github.com>
2018-04-30 20:50:24 -0500

    import

--- CEST/src/main/java/com/cest/Controllers/ControlExtintor.java
+++ CEST/src/main/java/com/cest/Controllers/ControlExtintor.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Controladores;
+package com.cest.Controllers;
 
-import Modelos.Extintor;
+import com.cest.Models.Extintor;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
