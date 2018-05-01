1a516818aabd8493209e31d972263d71ca1ca72f
santiago granada aguirre <32211963+santix96@users.noreply.github.com>
2018-04-30 20:18:23 -0500

    Import

--- CEST/src/main/java/com/cest/Dao/UsuarioDAO.java
+++ CEST/src/main/java/com/cest/Dao/UsuarioDAO.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package DAO;
-
-import Modelos.Usuario;
+package com.cest.Dao;
+import com.cest.Models.Usuario;
 
 /**
  * Conexion a base de datos Usuarios
