468782d2e84594ce07c2147de7eab3f7943d271b
santiago granada aguirre <32211963+santix96@users.noreply.github.com>
2018-04-30 19:56:03 -0500

    ExtintorDAO

--- CEST/src/main/java/com/cest/Dao/ExtintorDAO.java
+++ CEST/src/main/java/com/cest/Dao/ExtintorDAO.java
@@ -2,4 +2,42 @@ package com.cest.Dao;
 
 public class ExtintorDAO {
 
+    /**
+     * permite consultar en la base de datos la existencia de un extintor
+     * verificando que el identificador que ingresa coincida con alguno
+     * existente en la base de datos
+     *
+     * @param id identificador perteneciente al extintor
+     * @return Extintor que se encontro en la busqueda
+     */
+    public Extintor ConsultarExtintor(String id) {
+        return null;
+    }
+
+    /**
+     * Crea un nuevo registro de algun extintor en la base de datos
+     *
+     * @param extintor Extintor a ingresar
+     */
+    public void CrearExtintor(Extintor extintor) {
+
+    }
+
+    /**
+     * Modifica un registro de un extintor, reemplazandolo con otro (parametro)
+     *
+     * @param extintor
+     */
+    public void ModificarExtintor(Extintor extintor) {
+
+    }
+
+    /**
+     * Elimina un registro de un extintor, reemplazandolo con otro (parametro)
+     *
+     * @param extintor
+     */
+    public void EliminarrExtintor(Extintor extintor) {
+
+    }
 }
