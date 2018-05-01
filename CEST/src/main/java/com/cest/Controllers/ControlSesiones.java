560787c748a12c6ae6cf58d79e2eb02214a45b08
santiago granada aguirre <32211963+santix96@users.noreply.github.com>
2018-04-30 20:52:47 -0500

    import

--- CEST/src/main/java/com/cest/Controllers/ControlSesiones.java
+++ CEST/src/main/java/com/cest/Controllers/ControlSesiones.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Controladores;
+package com.cest.Controllers;
 
 import org.springframework.web.servlet.ModelAndView;
 
