6b9bd08c5de6dc9abbb1de0e438680d95e7f582e
santiago granada aguirre <32211963+santix96@users.noreply.github.com>
2018-04-30 20:27:33 -0500

    usuario

--- CEST/src/main/java/com/cest/Models/Usuario.java
+++ CEST/src/main/java/com/cest/Models/Usuario.java
@@ -6,7 +6,25 @@ import javax.persistence.Id;
 @Entity
 public class Usuario {
 	@Id
-	private String contrasena;
-	
-	private String usuario;
+	 private String encargado;
+    private String user;
+    private String password;
+    private String tipo;
+    private boolean login;
+
+    /**
+     * constructor parametrizable
+     *
+     * @param encargado
+     * @param user
+     * @param password
+     * @param tipo tipo de Usuario (adinistrador, encargado, usuario comun )
+     * @param login indica si el usuario se encuentra logueado
+     */
+    public Usuario(String encargado, String user, String password, String tipo, boolean login) {
+        this.encargado = encargado;
+        this.user = user;
+        this.password = password;
+        this.tipo = tipo;
+        this.login = login;F
 }
