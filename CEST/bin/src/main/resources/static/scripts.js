471d9a1d1cf4f30c50c4fff145547893bbba6d2c
mafe <maria.1701323228@ucaldas.edu.co>
2018-05-05 16:20:59 -0500

    .

--- CEST/bin/src/main/resources/static/scripts.js
+++ CEST/bin/src/main/resources/static/scripts.js
@@ -2,9 +2,27 @@ function ObtenerBloques(){
 	$.post( "/obtenerBloques",
 			{sede: $('#sede').val()},
 			function( data ) {
+				$('#bloque').empty();
+				$('#bloque').append('<option>Seleccione</option>');
 				$.each(data, 
 						function(i,val){
 							$('#bloque').append('<option>'+val+'</option>');
+						}
+				);
+			}
+	);
+}
+
+
+function ObtenerPisos(){
+	$.post( "/obtenerPisos",
+			{sede: $('#sede').val(), bloque: $('#bloque').val()},
+			function( data ) {
+				$('#piso').empty();
+				$('#piso').append('<option>Seleccione</option>');
+				$.each(data, 
+						function(i,val){
+							$('#piso').append('<option>'+val+'</option>');
 						}
 				);
 			}
