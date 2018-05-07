48ac40cf7ba557d4f094d8bfe22b44892d32cfef
Pablo <pablo.1701322631@ucaldas.edu.co>
2018-05-07 18:19:31 -0500

    actualizacion modificar extintor y clase

--- CEST/src/main/java/com/cest/Controllers/ExtintorController.java
+++ CEST/src/main/java/com/cest/Controllers/ExtintorController.java
@@ -1,11 +1,17 @@
 package com.cest.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.cest.Models.Extintor;
+import com.cest.Services.ExtintorService;
 
 
 
@@ -16,6 +22,9 @@ import com.cest.Models.Extintor;
 @RequestMapping
 public class ExtintorController {
 	
+	@Autowired
+	private ExtintorService extintorService;
+	
 
 	/*
 	 * Visualiza pagina index
@@ -24,6 +33,18 @@ public class ExtintorController {
 	public String getRegistrar(Model modelo) {
 		modelo.addAttribute("extintor", new Extintor());
 		return "registrarExtintor";
+	}
+	
+	@GetMapping(value = "/actualizarExtintor")
+	public String getActualizar(Model modelo) {
+		modelo.addAttribute("extintor", new Extintor());
+		return "actualizarExtintor";
+	}
+	
+	@PostMapping(value = "/actualizarExtintor")
+	public ModelAndView save(@ModelAttribute Extintor extintor) {
+		extintorService.update(extintor);
+		return new ModelAndView("redirect:/home");
 	}
 	
 }
