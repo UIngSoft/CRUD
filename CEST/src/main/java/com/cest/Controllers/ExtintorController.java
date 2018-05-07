10ff776a13489b16f6bdf6f690ddb61759fb5e00
luistrejos <34162422+luistrejos@users.noreply.github.com>
2018-05-07 11:52:16 -0500

    Cambios en registrar extintor

--- CEST/src/main/java/com/cest/Controllers/ExtintorController.java
+++ CEST/src/main/java/com/cest/Controllers/ExtintorController.java
@@ -1,11 +1,14 @@
 package com.cest.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cest.Dao.SedeDAO;
 import com.cest.Models.Extintor;
+import com.cest.Models.Sede;
 
 
 
@@ -15,7 +18,8 @@ import com.cest.Models.Extintor;
 @Controller
 @RequestMapping
 public class ExtintorController {
-	
+	@Autowired
+	private SedeDAO sedeDao;
 
 	/*
 	 * Visualiza pagina index
@@ -23,6 +27,7 @@ public class ExtintorController {
 	@GetMapping(value = "/registrarExtintor")
 	public String getRegistrar(Model modelo) {
 		modelo.addAttribute("extintor", new Extintor());
+		modelo.addAttribute("sedes", sedeDao.findAll());
 		return "registrarExtintor";
 	}
 	
