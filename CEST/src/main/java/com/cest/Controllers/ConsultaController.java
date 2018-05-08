b278e224666acd18c7ed7a8217e06a5f64584b4e
luistrejos <34162422+luistrejos@users.noreply.github.com>
2018-05-08 17:06:41 -0500

    .

--- CEST/src/main/java/com/cest/Controllers/ConsultaController.java
+++ CEST/src/main/java/com/cest/Controllers/ConsultaController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cest.Dao.ExtintorDAO;
-import com.cest.Models.Extintor;
 
 
 
