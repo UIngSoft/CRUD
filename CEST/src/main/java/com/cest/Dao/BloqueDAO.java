2ebf3fefa6e2023a7b61ab61bb6f06de91bcfa11
santix96 <32211963+santix96@users.noreply.github.com>
2018-05-07 23:17:58 -0500

    DAO piso, bloque
    adicion clases DAO piso y bloque

---
+++ CEST/src/main/java/com/cest/Dao/BloqueDAO.java
@@ -0,0 +1,8 @@
+package com.cest.Dao;
+import org.springframework.data.repository.CrudRepository;
+
+import com.cest.Models.Bloque;
+
+public interface BloqueDAO extends CrudRepository<Bloque, Character>{
+
+}
