d1c7c92cd612c32960656b0a3d56ccada02b66d2
Ovnyx <l.zuluaga94@gmail.com>
2018-05-07 23:07:44 -0500

    Agregado filtro por ubicacion en ConsultaController

--- CEST/src/main/java/com/cest/Controllers/ConsultaController.java
+++ CEST/src/main/java/com/cest/Controllers/ConsultaController.java
@@ -1,5 +1,8 @@
 package com.cest.Controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,8 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.cest.Dao.SedeDAO;
 import com.cest.Dao.ExtintorDAO;
+import com.cest.Dao.BloqueDAO;
+import com.cest.Dao.PisoDAO;
 import com.cest.Models.Extintor;
+import com.cest.Models.Sede;
+import com.cest.Models.Bloque;
+import com.cest.Models.Piso;
 
 
 
@@ -22,17 +31,58 @@ public class ConsultaController {
 	@Autowired
 	private ExtintorDAO extintorDao;
 	
+	@Autowired
+	private SedeDAO sedeDao;
+	
+	@Autowired
+	private BloqueDAO bloqueDao;
+	
+	@Autowired
+	private PisoDAO pisoDao;
+	
 	/*
 	 * Visualiza pagina de consultas
 	 */
 	@GetMapping(value = "/consulta")
-	public String getHome(Model modelo, @RequestParam String tipo, @RequestParam String sede, @RequestParam String bloque, @RequestParam String piso) {
-		System.out.println(sede);
+	public String getHome(Model modelo, @RequestParam String tipo, @RequestParam String nombresede, @RequestParam String nombrebloque, @RequestParam String nombrepiso) {
 		if (tipo.equals("general")) {
 			modelo.addAttribute("elementos", extintorDao.findAll());
 			return "consultaGeneral";
 		}else if (tipo.equals("extintor")) {
+			if(nombrepiso.equals("")) {
 			modelo.addAttribute("extintores", extintorDao.findAll());
+			modelo.addAttribute("sedes", sedeDao.findAll());
+			List<Bloque> bloques = new ArrayList<>();
+			List<Piso> pisos = new ArrayList<>();
+			if(!nombresede.equals("")) {
+				for (Bloque bloque: bloqueDao.findAll()) {
+					if (bloque.getSede().getNombre().equals(nombresede)) {
+						bloques.add(bloque);
+						
+					}
+				}
+				modelo.addAttribute("bloques", bloques);
+			}
+			if(!nombrebloque.equals("")) {
+				for (Piso piso: pisoDao.findAll()) {
+					if (piso.getBloque().getNombre().equals(nombrebloque)) {
+						pisos.add(piso);
+						
+					}
+				}
+				modelo.addAttribute("pisos", pisos);
+			}
+			}else {
+				List<Extintor> Extintores = new ArrayList<>();
+				for (Extintor extintor: extintorDao.findAll()) {
+					if (extintor.getElemento().getNumeropiso() == Integer.parseInt(nombrepiso)) {
+						Extintores.add(extintor);
+					}
+				}
+				modelo.addAttribute("extintores", Extintores);
+				modelo.addAttribute("sedes", sedeDao.findAll());
+			}
+			
 			return "consultaExtintor";
 		}else if (tipo.equals("botiquin")) {
 			return "consultaBotiquin";
@@ -40,5 +90,9 @@ public class ConsultaController {
 			return "consultaCamilla";
 		}
 	}
+	
+	
+	
+
 	
 }
