c89cb0ce55cdc5935a1f48f21d55c62c99461b72
luistrejos <34162422+luistrejos@users.noreply.github.com>
2018-05-14 14:42:31 -0500

    .

--- CEST/src/main/java/com/cest/Models/Piso.java
+++ CEST/src/main/java/com/cest/Models/Piso.java
@@ -11,14 +11,6 @@ import com.cest.LlavesCompuestas.PisoPK;
  */
 @Entity
 public class Piso{
-<<<<<<< HEAD
-=======
-	@Id
-	private int numero;
-	@ManyToOne
-	private Bloque bloque;
-	
->>>>>>> 8be1d27fecc6a72e2e298ffd0e927cd62c97d704
 	public Piso() {
 		// TODO Auto-generated constructor stub
 	}
