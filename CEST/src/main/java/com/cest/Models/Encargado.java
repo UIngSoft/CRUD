aa65e016f1466a9464da774b7e83d696af2df52f
Pablo <pablo.1701322631@ucaldas.edu.co>
2018-05-15 20:07:48 -0500

    actualizacion en Actuaizar Extintor

--- CEST/src/main/java/com/cest/Models/Encargado.java
+++ CEST/src/main/java/com/cest/Models/Encargado.java
@@ -12,4 +12,20 @@ public class Encargado {
 	private int cedula;
 	
 	private String nombre;
+
+	public int getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(int cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 }
