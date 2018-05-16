9d7f225264ce64058bb14437180c7b2b95e0eea1
luistrejos <34162422+luistrejos@users.noreply.github.com>
2018-05-15 23:11:41 -0500

    .

--- CEST/src/main/java/com/cest/Models/Encargado.java
+++ CEST/src/main/java/com/cest/Models/Encargado.java
@@ -13,42 +13,27 @@ public class Encargado {
 	
 	private String nombre;
 
-<<<<<<< HEAD
-=======
 	/**
 	 * @return the cedula
 	 */
->>>>>>> 669416525bb28225375f5c59b453c9fa08583460
 	public int getCedula() {
 		return cedula;
 	}
-
-<<<<<<< HEAD
-=======
 	/**
 	 * @param cedula the cedula to set
 	 */
->>>>>>> 669416525bb28225375f5c59b453c9fa08583460
 	public void setCedula(int cedula) {
 		this.cedula = cedula;
 	}
-
-<<<<<<< HEAD
-=======
 	/**
 	 * @return the nombre
 	 */
->>>>>>> 669416525bb28225375f5c59b453c9fa08583460
 	public String getNombre() {
 		return nombre;
 	}
-
-<<<<<<< HEAD
-=======
 	/**
 	 * @param nombre the nombre to set
 	 */
->>>>>>> 669416525bb28225375f5c59b453c9fa08583460
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
