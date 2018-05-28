a4883273f3b09b6ee1d1104bdab4ef9c929aa1dd
luistrejos <34162422+luistrejos@users.noreply.github.com>
2018-05-28 08:43:17 -0500

    Registrar reporte terminado

--- CEST/src/main/java/com/cest/Controllers/ReporteController.java
+++ CEST/src/main/java/com/cest/Controllers/ReporteController.java
@@ -1,12 +1,21 @@
 package com.cest.Controllers;
 
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.cest.Dao.ReporteDAO;
 import com.cest.Dao.SedeDAO;
+import com.cest.Models.Reporte;
 
 @Controller
 @RequestMapping
@@ -15,10 +24,31 @@ public class ReporteController {
 	@Autowired
 	private SedeDAO sedeDao;
 	
+	@Autowired
+	private ReporteDAO reporteDao;
+	
 	@GetMapping(value="/reportarElemento")
 	public String getReporte(Model modelo) {
 		modelo.addAttribute("sedes", sedeDao.findAll());
 		return "reporte";
+	}
+	
+	@PostMapping(value="/reportarElemento")
+	public ModelAndView postReporte(@RequestParam("tipoelemento") String tipoelemento
+			,@RequestParam("sede") String sede
+			,@RequestParam("bloque") String bloque
+			,@RequestParam("piso") String piso
+			,@RequestParam("descripcion") String descripcion) 
+	{
+		Reporte reporte = new Reporte();
+		reporte.setTipoelemento(tipoelemento);
+		Calendar fecha = new GregorianCalendar();
+		reporte.setFechareporte(LocalDate.of(fecha.get(Calendar.YEAR),fecha.get(Calendar.MONTH) + 1,fecha.get(Calendar.DAY_OF_MONTH)));
+		reporte.setDescripcion(descripcion);
+		reporte.setEstado("Pendiente");
+		reporte.setUbicacion("Sede: "+sede+" Bloque: "+bloque+" Piso: "+piso);
+		reporteDao.save(reporte);
+		return new ModelAndView("redirect:/");
 	}
 
 }
--- CEST/src/main/java/com/cest/Models/Reporte.java
+++ CEST/src/main/java/com/cest/Models/Reporte.java
@@ -1,6 +1,10 @@
 package com.cest.Models;
 
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /*
@@ -9,9 +13,115 @@ import javax.persistence.Id;
 @Entity
 public class Reporte {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
-	
-	private int idelemento;
-	
+	private String tipoelemento;
+	private LocalDate fechareporte;
 	private String descripcion;
+	private String argumento;
+	private String estado;
+	private String ubicacion;
+	
+	public Reporte() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the tipoelemento
+	 */
+	public String getTipoelemento() {
+		return tipoelemento;
+	}
+
+	/**
+	 * @param tipoelemento the tipoelemento to set
+	 */
+	public void setTipoelemento(String tipoelemento) {
+		this.tipoelemento = tipoelemento;
+	}
+
+	/**
+	 * @return the fechareporte
+	 */
+	public LocalDate getFechareporte() {
+		return fechareporte;
+	}
+
+	/**
+	 * @param fechareporte the fechareporte to set
+	 */
+	public void setFechareporte(LocalDate fechareporte) {
+		this.fechareporte = fechareporte;
+	}
+
+	/**
+	 * @return the descripcion
+	 */
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	/**
+	 * @param descripcion the descripcion to set
+	 */
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	/**
+	 * @return the argumento
+	 */
+	public String getArgumento() {
+		return argumento;
+	}
+
+	/**
+	 * @param argumento the argumento to set
+	 */
+	public void setArgumento(String argumento) {
+		this.argumento = argumento;
+	}
+
+	/**
+	 * @return the estado
+	 */
+	public String getEstado() {
+		return estado;
+	}
+
+	/**
+	 * @param estado the estado to set
+	 */
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	/**
+	 * @return the ubicacion
+	 */
+	public String getUbicacion() {
+		return ubicacion;
+	}
+
+	/**
+	 * @param ubicacion the ubicacion to set
+	 */
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+	
 }
