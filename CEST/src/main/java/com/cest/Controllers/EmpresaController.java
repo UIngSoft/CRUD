00f151993c4cf75064a52a164113965beeda150a
santix96 <32211963+santix96@users.noreply.github.com>
2018-06-24 04:50:42 -0500

    crud empresa

---
+++ CEST/src/main/java/com/cest/Controllers/EmpresaController.java
@@ -0,0 +1,119 @@
+package com.cest.Controllers;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.cest.Dao.EmpresaDAO;
+import com.cest.Dao.ReporteDAO;
+import com.cest.Dao.SedeDAO;
+import com.cest.Models.Empresa;
+import com.cest.Models.Reporte;
+
+@Controller
+@RequestMapping
+public class EmpresaController {
+	
+	
+	@Autowired
+	private EmpresaDAO empresaDao;
+
+	
+	
+	@GetMapping(value = "/crearEmpresa")
+	public List<Empresa> getRegistrarEmpresas(Model modelo) {
+		List<Empresa> empresas = new LinkedList<>();
+		modelo.addAttribute("empresas", empresaDao.findAll());
+		return empresas;
+	}
+	@GetMapping(value = "/crudEmpresa")
+	public List<Empresa> getConsultarEmpresas(Model modelo) {
+		List<Empresa> empresas = new LinkedList<>();
+		modelo.addAttribute("empresas", empresaDao.findAll());
+		return empresas;
+	}
+	
+	@GetMapping(value = "/modificarEmpresa")
+	public Empresa getModificarEmpresa(@RequestParam int nit) {
+		for (Empresa empresa : empresaDao.findAll()) {
+			if (empresa.getNit() == nit) {			
+				empresaDao.save(empresa);
+				return empresa;
+			}
+		}
+		return null;
+	}
+	
+
+	
+	@PostMapping(value = "/modificarEmpresa")
+	@ResponseBody
+	public ModelAndView postModificarEmpresa(@RequestParam("nit") int nit,
+			@RequestParam("direccion") String direccion,
+			@RequestParam("nombre") String nombre,
+			@RequestParam("telefono") int telefono
+			) {
+		
+		for (Empresa empresa1 : empresaDao.findAll()) {
+			if (empresa1.getNit() == nit) {
+				Empresa empresa = empresa1;
+				empresa.setDireccion(direccion);
+				empresa.setNombre(nombre);
+				empresa.setTelefono(telefono);
+				empresaDao.save(empresa);
+				
+				return new ModelAndView("redirect:/crudEmpresa");
+			}
+		}
+		
+		return null;
+	}
+	
+	
+	@PostMapping(value="/crearEmpresa")
+	public ModelAndView postRegistrarEmpresa(@RequestParam("nit") int nit
+			,@RequestParam("direccion") String direccion
+			,@RequestParam("nombre") String nombre
+			,@RequestParam("telefono") int telefono)
+			 
+	{
+		Empresa empresa = new Empresa();
+		empresa.setNit(nit);
+		empresa.setDireccion(direccion);
+		empresa.setNombre(nombre);
+		empresa.setTelefono(telefono);
+
+		empresaDao.save(empresa);
+		return new ModelAndView("redirect:/crudEmpresa");
+	}
+	
+	@GetMapping(value = "/crudEmpresaE")
+	public ModelAndView postEliminarEmpresa(@RequestParam("nit") int nit) {
+		
+		for (Empresa empresa : empresaDao.findAll()) {
+			if (empresa.getNit() == nit) {
+				empresaDao.delete(empresa);
+				return new ModelAndView("redirect:/crudEmpresa");
+			}
+		}
+		
+		return null;
+	}
+	 
+
+	
+
+	
+}
