54ad045f3604ef31f4f9dbb94199767414ca2345
luistrejos <34162422+luistrejos@users.noreply.github.com>
2018-06-29 10:42:58 -0500

    Mapeo Camilla

---
+++ CEST/src/main/java/com/cest/Controllers/CamillaController.java
@@ -0,0 +1,10 @@
+package com.cest.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class CamillaController {
+
+}
